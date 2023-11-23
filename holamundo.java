c32f9e0b511607ad2bc4be2130f3270993342e6d
Pablo <inventado1@gmail.com>
2023-11-23 23:48:31 +0100

    Modifico para que salga hola mundo

--- holamundo.java
+++ holamundo.java
@@ -4,8 +4,6 @@ import java.util.Scanner;
 
 public class holamundo {
     public static void main(String[] args) {
- int resultado = 10/1;
- System.out.println(resultado);
- Scanner scanner = new Scanner(System.in);
+ System.out.println("Hola mundo");
   }
     }
