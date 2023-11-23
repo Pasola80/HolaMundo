fc584f58873b342be6f40a2786cfe84fd6d3ea30
Pablo <inventado1@gmail.com>
2023-11-23 23:43:58 +0100

    añado fichero hola mundo

---
+++ holamundo.java
@@ -0,0 +1,11 @@
+package holamundo;
+
+import java.util.Scanner;
+
+public class holamundo {
+    public static void main(String[] args) {
+ int resultado = 10/1;
+ System.out.println(resultado);
+ Scanner scanner = new Scanner(System.in);
+  }
+    }
